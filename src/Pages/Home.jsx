f7d4abd4cb6f3fe7c7159fce3dbad89a5adca206
Vu Huy <vulehuy0102@gmail.com>
2024-05-24 21:08:01 +0700

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection.jsx'
+import About from '../components/About.jsx'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection/>
+    <About/>
+    </>
+  )
+}
+
+export default Home
