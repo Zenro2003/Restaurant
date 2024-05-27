bbc215f717828a539ff952a0178fb4d588492e43
Vu Huy <vulehuy0102@gmail.com>
2024-05-27 09:25:09 +0700

    do complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import HeroSection from '../components/HeroSection.jsx'
 import About from '../components/About.jsx'
+import HeroSection from '../components/HeroSection.jsx'
+import Quanlities from '../components/Quanlities.jsx'
+import Menu from '../components/Menu.jsx'
+import WhoAreWe from './../components/WhoAreWe';
+import Team from '../components/Team.jsx'
+import Reservation from '../components/Reservation.jsx'
+import Footer from '../components/Footer.jsx'
+
 
 const Home = () => {
   return (
     <>
     <HeroSection/>
     <About/>
+    <Quanlities/>
+    <Menu/>
+    <WhoAreWe/>
+    <Team/>
+    <Reservation/>
+    <Footer/>
     </>
   )
 }
